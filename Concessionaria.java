30613c2c66ec7df02b73c764eb43a603da4be396
Guilherme Washington <100482900+GuilhermeGwsS@users.noreply.github.com>
2023-03-06 15:57:16 -0300

    Add files via upload

---
+++ Concessionaria.java
@@ -0,0 +1,31 @@
+package Pratica1;
+public class Concessionaria 
+{
+	public class Carro
+	{
+		String modelo;     //ATRIBUTOS
+		String nomeCarro;  //ATRIBUTOS
+		String cor;       //ATRIBUTOS
+		double kmRodados;     //ATRIBUTOS
+		double peso;        //ATRIBUTOS
+		String tamanhoRodas; //ATRIBUTOS
+		boolean ligado;  //ATRIBUTOS	
+	}
+	public class Cliente
+	{
+		String nome;     //ATRIBUTOS
+		String endereço;  //ATRIBUTOS
+		int idade;       //ATRIBUTOS
+		int CPF;     //ATRIBUTOS
+		String formaPagamento;  //ATRIBUTOS	
+		
+	}
+	public class Transação
+	{
+		double valor;     //ATRIBUTOS
+		String vender;  //ATRIBUTOS
+		String comprar;  //ATRIBUTOS
+		String formaPagamento;  //ATRIBUTOS	
+	}
+	
+}
---
+++ EmpregoFaculdade.java
@@ -0,0 +1,18 @@
+package Pratica1;
+
+public class EmpregoFaculdade 
+{
+		private String nome;
+		private double salario;
+		private int horasAula;
+		double getGastos()
+		{
+		return this.salario;
+		}
+		String getInfo()
+		{
+		return "nome: " + this.nome + "com salário " + this.salario;
+		}		
+
+
+}
